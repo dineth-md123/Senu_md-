18716a35f4a835f0e26650e1f960ad4555e0db58
dineth-md123 <dinethsenura400@gmail.com>
2025-02-15 11:11:30 +0530

    Update auto.js

--- plugins/auto.js
+++ plugins/auto.js
@@ -7,16 +7,16 @@ const {cmd , commands} = require('../command')
 cmd({
   on: "body"
 },    
-async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autovoice.json');
+async (senu, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autovoice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             const config = await readEnv();
             if (config.AUTO_VOICE === 'true') {
                 //if (isOwner) return;        
-                await robin.sendPresenceUpdate('recording', from);
-                await robin.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+                await senu.sendPresenceUpdate('recording', from);
+                await senu.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
             }
         }
     }                
@@ -26,15 +26,15 @@ async (robin, mek, m, { from, body, isOwner }) => {
 cmd({
   on: "body"
 },    
-async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autosticker.json');
+async (senu, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autosticker.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             const config = await readEnv();
             if (config.AUTO_STICKER === 'true') {
                 //if (isOwner) return;        
-                await robin.sendMessage(from,{sticker: { url : data[text]},package: 'S_I_H_I_L_E_L'},{ quoted: mek })   
+                await senu.sendMessage(from,{sticker: { url : data[text]},package: 'S_I_H_I_L_E_L'},{ quoted: mek })   
             
             }
         }
@@ -45,8 +45,8 @@ async (robin, mek, m, { from, body, isOwner }) => {
 cmd({
   on: "body"
 },    
-async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autoreply.json');
+async (senu, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autoreply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
