b1f5c9c6b8606e75f9c410912d16765e9de998c5
dineth-md123 <dinethsenura400@gmail.com>
2025-02-18 07:49:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9474xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "SENU-MD-V1AtBh2QRA#tUWuz1OpOv3aJWxMqvJej_WIW7nh71HOp5cslSY-VOc",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:opZoxyRXmtsolWSLkejYSnPealRPQkia@yamabiko.proxy.rlwy.net:22620",
+  OWNER_NUM: process.env.OWNER_NUM || "94743008953",
 };
